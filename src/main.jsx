ee2f128b3e046414b89c5b3e61f74615d5ed7404
moha-elh <mohsineelhaouary@gmail.com>
2026-06-24 21:19:18 +0100

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { CursorProvider } from './context/CursorContext';
 import './styles/globals.css';
 import App from './App';
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CursorProvider>
       <App />
+      <Analytics/>
     </CursorProvider>
   </StrictMode>
 );
